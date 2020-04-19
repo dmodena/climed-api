bfc0fb2057b916c6b9b857c01cfd823243fc48cc
Douglas Modena <d.modena@live.com>
2020-04-19 15:59:52 -0300

    Create role model

---
+++ CliMed.Api/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CliMed.Api.Models
+{
+    [Table("roles")]
+    public class Role
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("value")]
+        public string Value { get; set; }
+    }
+}
