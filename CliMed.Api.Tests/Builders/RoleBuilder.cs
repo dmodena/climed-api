bd0ca1cffa448fc72ad7f3cce3c98a27ed04d74c
Douglas Modena <d.modena@live.com>
2020-05-11 13:55:25 -0300

    Create object builders for tests

---
+++ CliMed.Api.Tests/Builders/RoleBuilder.cs
@@ -0,0 +1,42 @@
+﻿using CliMed.Api.Models;
+
+namespace CliMed.Api.Tests.Builders
+{
+    public class RoleBuilder
+    {
+        private readonly Role _object = new Role();
+
+        public Role Build()
+        {
+            return _object;
+        }
+
+        public static RoleBuilder Default()
+        {
+            return new RoleBuilder();
+        }
+
+        public static RoleBuilder Simple()
+        {
+            return Default()
+                .WithValue("admin");
+        }
+
+        public static RoleBuilder Typical()
+        {
+            return Simple();
+        }
+
+        public RoleBuilder WithId(long id)
+        {
+            _object.Id = id;
+            return this;
+        }
+
+        public RoleBuilder WithValue(string value)
+        {
+            _object.Value = value;
+            return this;
+        }
+    }
+}
---
+++ CliMed.Api.Tests/Builders/UserBuilder.cs
@@ -0,0 +1,71 @@
+﻿using CliMed.Api.Models;
+
+namespace CliMed.Api.Tests.Builders
+{
+    public class UserBuilder
+    {
+        private readonly User _object = new User();
+
+        public User Build()
+        {
+            return _object;
+        }
+
+        public static UserBuilder Default()
+        {
+            return new UserBuilder();
+        }
+
+        public static UserBuilder Simple()
+        {
+            return Default()
+                .WithEmail("a@a.com")
+                .WithPassword("a");
+        }
+
+        public static UserBuilder Typical()
+        {
+            return Simple()
+                .WithUsername("a")
+                .WithRole(RoleBuilder.Simple().Build());
+        }
+
+        public UserBuilder WithId(long id)
+        {
+            _object.Id = id;
+            return this;
+        }
+
+        public UserBuilder WithUsername(string username)
+        {
+            _object.Username = username;
+            return this;
+        }
+
+        public UserBuilder WithEmail(string email)
+        {
+            _object.Email = email;
+            return this;
+        }
+
+        public UserBuilder WithPassword(string password)
+        {
+            _object.Password = password;
+            return this;
+        }
+
+        public UserBuilder WithRoleId(long roleId)
+        {
+            _object.RoleId = roleId;
+            _object.Role = null;
+            return this;
+        }
+
+        public UserBuilder WithRole(Role role)
+        {
+            _object.Role = role;
+            _object.RoleId = role.Id;
+            return this;
+        }
+    }
+}
