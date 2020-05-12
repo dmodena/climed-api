a47ac10e4251b9ca0f47203486e6c29ca4344f34
Douglas Modena <d.modena@live.com>
2020-05-12 20:23:36 -0300

    Create tests for services

---
+++ CliMed.Api.Tests/Services/RoleServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using CliMed.Api.Models;
+using CliMed.Api.Repositories;
+using CliMed.Api.Services;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CliMed.Api.Tests.Services
+{
+    public class RoleServiceTests
+    {
+        private Mock<IRoleRepository> roleRepositoryMock;
+        private RoleService sut;
+
+        public RoleServiceTests()
+        {
+            roleRepositoryMock = new Mock<IRoleRepository>();
+            roleRepositoryMock.Setup(m => m.GetlAllItems()).Returns(new List<Role>());
+
+            sut = new RoleService(roleRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void GetAllItems_ShouldReturnRoleList()
+        {
+            var result = sut.GetAllItems();
+
+            Assert.IsAssignableFrom<IList<Role>>(result);
+        }
+    }
+}
---
+++ CliMed.Api.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using CliMed.Api.Models;
+using CliMed.Api.Repositories;
+using CliMed.Api.Services;
+using CliMed.Api.Tests.Builders;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CliMed.Api.Tests.Services
+{
+    public class UserServiceTests
+    {
+        private Mock<IUserRepository> userRepositoryMock;
+        private UserService sut;
+
+        public UserServiceTests()
+        {
+            userRepositoryMock = new Mock<IUserRepository>();
+            userRepositoryMock.Setup(m => m.GetAllItems()).Returns(new List<User>());
+            userRepositoryMock.Setup(m => m.GetById(It.IsAny<long>())).Returns(new User());
+            userRepositoryMock.Setup(m => m.GetByEmail(It.IsAny<string>())).Returns(new User());
+            userRepositoryMock.Setup(m => m.Create(It.IsAny<User>())).Returns(new User());
+
+            sut = new UserService(userRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void GetAllItems_ShouldReturnUserList()
+        {
+            var result = sut.GetAllItems();
+
+            Assert.IsAssignableFrom<IList<User>>(result);
+        }
+
+        [Fact]
+        public void GetById_ShouldReturnUser()
+        {
+            var result = sut.GetById(1);
+
+            Assert.IsType<User>(result);
+        }
+
+        [Fact]
+        public void GetByEmail_ShouldReturnUser()
+        {
+            var result = sut.GetByEmail("a@a.com");
+
+            Assert.IsType<User>(result);
+        }
+
+        [Fact]
+        public void Create_ShouldReturnUser()
+        {
+            var user = UserBuilder.Simple().Build();
+
+            var result = sut.Create(user);
+
+            Assert.IsType<User>(result);
+        }
+    }
+}
