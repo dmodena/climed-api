dc815df80fb45c6f218fff9edb85cd337c575b84
Douglas Modena <d.modena@live.com>
2020-05-30 11:59:15 -0300

    Remove singleton from data context for repository tests

--- CliMed.Api.Tests/Data/InMemoryDataContext.cs
+++ CliMed.Api.Tests/Data/InMemoryDataContext.cs
@@ -7,39 +7,34 @@ namespace CliMed.Api.Tests.Data
 {
     public class InMemoryDataContext : DataContext
     {
-        private static InMemoryDataContext _instance;
-
         private InMemoryDataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public static DataContext GetInstance()
         {
-            if (_instance != null)
-                return _instance;
-
             var options = new DbContextOptionsBuilder<DataContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
 
-            _instance = new InMemoryDataContext(options);
+            var context = new InMemoryDataContext(options);
 
-            Seed();
+            Seed(context);
 
-            return _instance;
+            return context;
         }
 
-        private static void Seed()
+        private static void Seed(InMemoryDataContext context)
         {
             var adminRole = new Role { Id = 1, Value = "admin" };
 
-            _instance.Roles.Add(adminRole);
+            context.Roles.Add(adminRole);
 
-            _instance.Users.AddRange(
+            context.Users.AddRange(
                 new[]
                 {
                     new User { Id = 1, Email = "john.doe@example.com", Password = "johndoe", Username = "johndoe" },
                     new User { Id = 2, Email = "admmin@example.com", Password = "admin", Username = "mradmin", RoleId = 1, Role = adminRole }
                 });
 
-            _instance.SaveChanges();
+            context.SaveChanges();
         }
     }
 }
